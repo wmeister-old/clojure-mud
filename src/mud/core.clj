0efdff8a9451f0c28999f3e0e90a21c97c6962da
William Meister <wmeister86@gmail.com>
2012-07-14 08:33:06 -0500

    refactored client syncing process

--- src/mud/core.clj
+++ src/mud/core.clj
@@ -13,14 +13,13 @@
 
 (defn sync-client [socket]
   (println "Attempting to sync client socket:" socket)
-  (dosync
-   (last
-    (alter clients
-           conj
-           (struct client
-                   socket
-                   (BufferedReader. (InputStreamReader. (.getInputStream socket)))
-                   (PrintWriter. (.getOutputStream socket) true))))))
+  (time (let [new-client (struct client
+                                 socket
+                                 (BufferedReader. (InputStreamReader. (.getInputStream socket)))
+                                 (PrintWriter. (.getOutputStream socket) true))]
+          (dosync (alter clients conj new-client))
+          (println "Finished syncing client:" socket)
+          new-client)))
 
 (defn parse-client-input [client input]
   (println (str "Received input from client: " (client :socket) " said \"" input "\"")))
@@ -50,16 +49,17 @@
          (println "Failed to accept client connection."))))
 
 (defn server []
-  (.start (Thread.
-           #(let [port 6689]
-               (try (let [socket (ServerSocket. port)]
-                      (println "Server bound to port " port ".")
-                      (println "Connection accepting thread is: " (Thread/currentThread))
-                      (while @keep-server-alive (await-connection socket))
-                      (println "Cleaning up socket on port " port " and exiting.")
-                      (.close socket))
-                    (catch IOException ex
-                      (println "Server could not bind to port " port ".")))))))
+  (.start
+   (Thread.
+    #(let [port 6689]
+       (try (let [socket (ServerSocket. port)]
+              (println "Server bound to port " port ".")
+              (println "Connection accepting thread is: " (Thread/currentThread))
+              (while @keep-server-alive (await-connection socket))
+              (println "Cleaning up socket on port " port " and exiting.")
+              (.close socket))
+            (catch IOException ex
+              (println "Server could not bind to port " port ".")))))))
 
 (defn -main
   "I don't do a whole lot."
